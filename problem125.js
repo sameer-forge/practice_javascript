7cdd61da1d11c1fb2b909a98b6db71ba7cc2e4da
sameerkpy <sameer.dev0740@gmail.com>
2026-07-10 21:20:14 +0530

    coding problems

---
+++ problem125.js
@@ -0,0 +1,71 @@
+// question 1
+// method 1
+// let a = 10 
+// let b = 5 
+
+// a = a + b;
+// b = a - b;
+// a = a -b;
+
+// console.log (a);
+// console.log(b)
+
+// method 2  destructuring asignment
+// let a = 10;
+// let b = 5;
+
+// [a, b] = [b, a];
+
+// console.log(a);
+// console.log(b);
+
+// method 3 temp use
+
+// let a = 10;
+// let b = 5;
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a)
+// console.log(b)
+
+// question 2 
+
+// const student = {
+//     name: "Sameer",
+//     age: 20
+// };
+
+// student.name = "Dinesh";
+
+// console.log(student);
+
+// add
+
+// const student = {
+//     name :"sameer"
+// };
+
+// student.city = "jaipur";
+
+// console.log(student);
+
+// nested objects
+
+// const student = {
+//     name: "sameer",
+//     address: {
+//         city: "jaipur"
+//     }
+// };
+
+// student.address.city = "mumbai"
+// console.log(student)
+
+// array
+
+// const numbers = [10,20,30]
+
+// numbers.push (40);
+// console.log(numbers);
